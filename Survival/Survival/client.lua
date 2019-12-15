6db1b6247caed5acf8adae4e94797b90c2fc1794
alexander-sand <48335642+alexander-sand@users.noreply.github.com>
2019-12-15 11:24:30 -0700

    Delete client.lua

--- Survival/Survival/client.lua
+++
@@ -1,8 +0,0 @@
---make a hunger bar first
---[[
-    Add player property value called hunger
-    Report value in chat
-]]
-AddEvent("OnPlayerStreamIn", function(player)
-    AddPlayerChat(player, "P: "..GetPlayerPropertyValue(player, "testValue"))
-end)
