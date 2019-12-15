441a2afc2426512a0610425f2149bf5d36fe3aa6
alexander-sand <48335642+alexander-sand@users.noreply.github.com>
2019-12-15 11:24:43 -0700

    Delete server.lua

--- Survival/Survival/server.lua
+++
@@ -1,7 +0,0 @@
-AddEvent("OnPlayerJoin", function(player)
-    SetPlayerPropertyValue(player, "testValue", player + 1, true)
-    AddPlayerChat(player, "P: "..GetPlayerPropertyValue(player, "testValue"))
-    print("testValue")
-end)
-
-print("Survival package loaded!")
