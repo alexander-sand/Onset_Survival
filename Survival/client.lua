f89ba9aaec3236c2ce7eed91c3e0697f3bc69e64
alexander-sand <48335642+alexander-sand@users.noreply.github.com>
2019-12-15 22:20:09 -0700

    Update client.lua
    Got started on drawing the hunger value to the players screen

--- Survival/client.lua
+++ Survival/client.lua
@@ -3,6 +3,4 @@
     Add player property value called hunger
     Report value in chat
 ]]
-AddEvent("OnPlayerStreamIn", function(player)
-    AddPlayerChat(player, "P: "..GetPlayerPropertyValue(player, "testValue"))
-end)
+DrawText(100, 100, "Hunger")
