7fce27d7d769f299e868f6881380217ba56e0a75
alexander-sand <48335642+alexander-sand@users.noreply.github.com>
2019-12-15 22:19:24 -0700

    Update server.lua
    Made it so that there is a hunger variable that can increase or decrease over time

--- Survival/server.lua
+++ Survival/server.lua
@@ -1,7 +1,26 @@
 AddEvent("OnPlayerJoin", function(player)
-    SetPlayerPropertyValue(player, "testValue", player + 1, true)
-    AddPlayerChat(player, "P: "..GetPlayerPropertyValue(player, "testValue"))
-    print("testValue")
+    local hunger_rate = 5000 --milliseconds
+    local hunger_web = nil
+
+    --Set initial player hunger value
+    SetPlayerPropertyValue(player, "hunger", 100, true)
+    AddPlayerChat(player, "P: "..GetPlayerPropertyValue(player, "hunger"))
+
+    --Decrease hunger at constant rate
+    CreateTimer(DecreaseHunger, hunger_rate, player)
+    
+    DrawText(100, 100, "Hunger")
 end)
+
+--Make players more hungery over time
+function DecreaseHunger(player)
+    --Put player hunger value into variable
+    local hunger = GetPlayerPropertyValue(player, "hunger")
+
+    --Decrease player hunger value
+    SetPlayerPropertyValue(player, "hunger", hunger - 1, true)
+    --Display hunger in chat
+    AddPlayerChat(player, "P: "..GetPlayerPropertyValue(player, "hunger"))
+end
 
 print("Survival package loaded!")
